62e00e9b55c9ba9a7ae23106595b67bb9cb0b6eb
Wayne Kuo <wayne.k@typuslab.com>
2026-01-08 11:54:15 +0800

    update

--- @WIP/dydx/src/abiFormatConverter.js
+++
@@ -1,8 +0,0 @@
-const ethers = require('ethers')
-humanReadableAbi =  [
-    "event Work(uint256 indexed id, uint256 loan)"
-  ]
-const iface = new ethers.utils.Interface(humanReadableAbi);
-jsonAbi = iface.format(ethers.utils.FormatTypes.json);
-
-console.log(JSON.stringify(JSON.parse(jsonAbi), null, 2))
